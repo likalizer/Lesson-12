f99c295c70248b70c5281496c4192296505f558d
likalizer <lika.lika1823@gmail.com>
2024-05-20 22:30:02 +0300

    Завдання для PlaylistByGenreViewController додано

--- Lesson12HW/Source/Module/PlaylistByGenreScreen/PlaylistByGenreModel.swift
+++ Lesson12HW/Source/Module/PlaylistByGenreScreen/PlaylistByGenreModel.swift
@@ -7,12 +7,36 @@
 
 import Foundation
 
+
 protocol PlaylistByGenreModelDelegate: AnyObject {
-    
+    func dataDidLoad()
 }
 
 class PlaylistByGenreModel {
-    
     weak var delegate: PlaylistByGenreModelDelegate?
     private let dataLoader = DataLoaderService()
+    
+    var songsByGenre: [String: [Song]] = [:] 
+    
+    func loadData() {
+        dataLoader.loadPlaylist { [weak self] playlist in
+            guard let playlist = playlist else { return }
+            
+            var groupedSongs: [String: [Song]] = [:]
+            for song in playlist.songs {
+                if var songs = groupedSongs[song.genre] {
+                    songs.append(song)
+                    groupedSongs[song.genre] = songs
+                } else {
+                    groupedSongs[song.genre] = [song]
+                }
+            }
+            
+            self?.songsByGenre = groupedSongs
+            
+            DispatchQueue.main.async {
+                self?.delegate?.dataDidLoad()
+            }
+        }
+    }
 }
--- Lesson12HW/Source/Module/PlaylistByGenreScreen/PlaylistByGenreViewController.swift
+++ Lesson12HW/Source/Module/PlaylistByGenreScreen/PlaylistByGenreViewController.swift
@@ -10,15 +10,63 @@ import UIKit
 class PlaylistByGenreViewController: UIViewController {
     
     @IBOutlet weak var contentView: PlaylistByGenreView!
+   
+    
     var model: PlaylistByGenreModel!
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        setupInitialState()
-    }
-    
-    private func setupInitialState() {
         
-        model = PlaylistByGenreModel()
+        override func viewDidLoad() {
+            super.viewDidLoad()
+            setupInitialState()
+            loadData()
+        }
+        
+        private func setupInitialState() {
+            model = PlaylistByGenreModel()
+            model.delegate = self
+            contentView.tableView.dataSource = self
+            contentView.tableView.delegate = self
+            contentView.tableView.register(UITableViewCell.self, forCellReuseIdentifier: "SongCell")
+        }
+        
+        private func loadData() {
+            model.loadData()
+        }
     }
-}
+
+    extension PlaylistByGenreViewController: PlaylistByGenreModelDelegate {
+        func dataDidLoad() {
+            DispatchQueue.main.async {
+                self.contentView.tableView.reloadData()
+            }
+        }
+    }
+
+    extension PlaylistByGenreViewController: UITableViewDataSource {
+        func numberOfSections(in tableView: UITableView) -> Int {
+            return model.songsByGenre.count
+        }
+        
+        func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+            let genre = model.songsByGenre.keys.sorted()[section]
+            return model.songsByGenre[genre]?.count ?? 0
+        }
+        
+        func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+            let cell = tableView.dequeueReusableCell(withIdentifier: "SongCell", for: indexPath)
+            let genre = model.songsByGenre.keys.sorted()[indexPath.section]
+            if let songs = model.songsByGenre[genre] {
+                let song = songs[indexPath.row]
+                cell.textLabel?.text = "\(song.songTitle) - \(song.author)"
+            }
+            return cell
+        }
+        
+        func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+            return model.songsByGenre.keys.sorted()[section]
+        }
+    }
+
+    extension PlaylistByGenreViewController: UITableViewDelegate {
+       
+    }
+
